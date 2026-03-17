47ecffefad215aa4e5a913772abfd14e2d87d96e
Deb Mohan <dmohan@intraedge.com>
2026-03-16 23:43:49 -0700

    chore: regenerate backend.tf with epoch-based storage account name

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate-iemlops-eastus"
-    storage_account_name = "tfstate11669"
+    storage_account_name = "tfstate1773729419"
     container_name       = "tfstate"
     use_azuread_auth     = true
     # key is provided at init time:
